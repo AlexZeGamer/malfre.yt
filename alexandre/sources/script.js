0bb9abfb4c24e21852bd5d647b9045a54315926e
AlexZeGamer <alexandre.malfreyt@gmail.com>
2022-01-17 00:50:14 +0100

    Projects

--- alexandre/sources/script.js
+++ alexandre/sources/script.js
@@ -19,19 +19,14 @@ function getAnecdote() {
     request.onload = function () {
         let anecdote = request.response[Math.floor(Math.random() * request.response.length)];
 
-        console.log(anecdote);
-
         document.getElementById('anecdote-text').innerHTML = anecdote['Texte'];
         document.getElementById('anecdote-emoji').innerHTML = anecdote['Emoji'];
         document.getElementById('anecdote-en_savoir_plus').setAttribute('href', anecdote['En savoir plus']);
-
-        console.log(anecdote['Texte'])
-        console.log(anecdote['Emoji'])
-        console.log(anecdote['En savoir plus'])
     }
 }
 
 getAnecdote();
+
 
 function getAge() {
     let now = new Date();
@@ -44,3 +39,33 @@ function getAge() {
 }
 
 getAge();
+
+
+function getProjects() {
+    let fileURL = "./sources/projects.json";
+    let request = new XMLHttpRequest();
+    request.open('GET', fileURL);
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = function () {
+        for (let i = 0; i < request.response.length; i++) {
+            let projects_div = document.getElementById('projects-timeline');
+
+            var div = document.createElement('div');
+            div.classList.add('project-div')
+
+            var a = document.createElement('a');
+            var title = document.createElement('h3');
+            var text = document.createTextNode(request.response[i]['Name']);
+            title.appendChild(text);
+            a.appendChild(title);
+            a.setAttribute('href', request.response[i]['Lien']);
+            
+            div.appendChild(a);
+            projects_div.appendChild(div);
+        }
+    }
+}
+
+getProjects();
